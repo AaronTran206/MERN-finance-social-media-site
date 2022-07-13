219b460c0c7dad1e6cae74ae6b6ad5d921f010aa
AaronTran206 <aarontran.wa@gmail.com>
2022-07-13 14:04:29 -0600

    grammar error in search bar label

--- client/src/components/searchBar/SearchBar.tsx
+++ client/src/components/searchBar/SearchBar.tsx
@@ -7,30 +7,6 @@ const SearchBar: React.FC<{}> = () => {
   const [ticker, setTicker] = useState<string>("")
   const dispatch = useAppDispatch()
 
-  // useEffect(() => {
-
-  //     // //sort historical data from oldest to newest to display a more readable graph and cut the data because an overload of data makes the graphs hard to read and analyze
-  //     // const sortedRes = {
-  //     //   historical: res.data.stock.historical
-  //     //     ?.sort(
-  //     //       (a: any, b: any) =>
-  //     //         new Date(a.date).getTime() - new Date(b.date).getTime()
-  //     //     )
-  //     //     .splice(
-  //     //       res.data.stock.historical.length - 400,
-  //     //       res.data.stock.historical.length
-  //     //     ),
-  //     //   symbol: res.data.stock.symbol,
-  //     // }
-
-  //     // //set filtered and organized result to state data
-  //     // setStockData(sortedRes)
-  //     // setRatingData(res.data.rating[0])
-  //     // setFinData(res.data.fin)
-  //     // setMktCapData(res.data.mktData[0])
-  //   })
-  // }, [])
-
   const onSubmit = (e: any) => {
     e.preventDefault()
 
@@ -46,7 +22,7 @@ const SearchBar: React.FC<{}> = () => {
             <TextField
               name="ticker"
               variant="outlined"
-              label="Search a Ticker"
+              label="Search for a Ticker"
               onChange={(e) => setTicker(e.target.value)}
               fullWidth
             />
